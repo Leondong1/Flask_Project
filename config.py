982fea5fcb25d9c5c3f560907f0edab109af823b
Leon <wangdongjie1994@gmail.com>
2019-05-25 20:01:15 +0800

    将项目中的配置信息单独抽取到config.py文件中

---
+++ config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2019-05-25 19:51
+@Author  : Leon
+@Contact : wangdongjie1994@gmail.com
+@File    : config.py
+@Software: PyCharm
+'''
+import redis
+
+
+class Config(object):
+    """工程的配置信息"""
+    DEBUG = True
+
+    SECRET_KEY = "abcdefghij"
+
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:dongjie931207@127.0.0.1:3306/news_information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # redis 配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    # flask_session 的配置信息
+    SESSION_TYPE = "redis"
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,
+                                      port=REDIS_PORT)
+    PERMANENT_SESSION_LIFETIME = 86400
+
+
--- manage.py
+++ manage.py
@@ -13,28 +13,13 @@ from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_session import Session
+from config import Config
 
 app = Flask(__name__)
 
 manager = Manager(app)
 
 
-
-class Config(object):
-    """工程的配置信息"""
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:dongjie931207@127.0.0.1:3306/news_information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # redis 配置
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    # flask_session 的配置信息
-    SESSION_TYPE = "redis"
-    SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,
-                                      port=REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 86400
 
 
 app.config.from_object(Config)
